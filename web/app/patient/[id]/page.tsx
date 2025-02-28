ebd72eaac90baa1cd08aedc53c20228ce73c34da
Ben Sun <sunb26@mcmaster.ca>
2025-02-28 16:05:59 -0500

    web: mock get patient endpoint

--- web/app/patient/[id]/page.tsx
+++ web/app/patient/[id]/page.tsx
@@ -3,91 +3,99 @@ import { DataTable } from "@/components/ui/recordings/data-table";
 import Image from "next/image";
 import React from "react";
 
-
-const Patient = {
-  firstName: "John",
-  lastName: "Doe",
-  email: "example@gmail.com",
-  age: 25,
-  sex: "M",
-  weight: "160 lbs",
-  height: "180 cm",
+type Patient = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  age: number;
+  sex: string;
+  weight: string;
+  height: string;
+  recordings: Recording[];
 };
 
-async function getRecordings(): Promise<Recording[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: 1,
-      date: "2024-02-21T14:30:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 2,
-      date: "2024-02-20T09:15:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 3,
-      date: "2024-02-19T16:45:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 4,
-      date: "2024-02-18T11:20:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 5,
-      date: "2024-02-17T13:00:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 6,
-      date: "2024-02-16T15:30:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 7,
-      date: "2024-02-15T10:45:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 8,
-      date: "2024-02-14T12:15:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 9,
-      date: "2024-02-13T09:00:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 10,
-      date: "2024-02-12T14:20:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 11,
-      date: "2024-02-11T16:30:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 12,
-      date: "2024-02-10T11:45:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    {
-      id: 13,
-      date: "2024-02-09T10:00:00Z",
-      url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
-    },
-    // ...
-  ];
+async function getPatient(): Promise<Patient> {
+  return {
+    firstName: "John",
+    lastName: "Doe",
+    email: "example@gmail.com",
+    age: 25,
+    sex: "M",
+    weight: "160 lbs",
+    height: "180 cm",
+    recordings: [
+      {
+        id: 1,
+        date: "2024-02-21T14:30:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 2,
+        date: "2024-02-20T09:15:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 3,
+        date: "2024-02-19T16:45:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 4,
+        date: "2024-02-18T11:20:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 5,
+        date: "2024-02-17T13:00:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 6,
+        date: "2024-02-16T15:30:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 7,
+        date: "2024-02-15T10:45:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 8,
+        date: "2024-02-14T12:15:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 9,
+        date: "2024-02-13T09:00:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 10,
+        date: "2024-02-12T14:20:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 11,
+        date: "2024-02-11T16:30:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 12,
+        date: "2024-02-10T11:45:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      {
+        id: 13,
+        date: "2024-02-09T10:00:00Z",
+        url: "https://firebasestorage.googleapis.com/v0/b/heartlink-6fee0.firebasestorage.app/o/recordings%2Fchillin39-20915.wav?alt=media&token=7ac4ed0a-d427-47cc-9172-ca727c596179",
+      },
+      // ...
+    ],
+  };
 }
 
 export default async function PatientPage() {
-  const recordings = await getRecordings();
+  const patient = await getPatient();
   return (
     <div>
       <div className="bg-off-white">
@@ -102,25 +110,25 @@ export default async function PatientPage() {
             />
           </div>
           <p className="text-3xl text-left py-10 font-[Syne] whitespace-pre-line">
-            First Name: {Patient.firstName}
+            First Name: {patient.firstName}
             <br />
-            Last Name: {Patient.lastName}
+            Last Name: {patient.lastName}
             <br />
-            Email: {Patient.email}
+            Email: {patient.email}
             <br />
-            Age: {Patient.age}
+            Age: {patient.age}
             <br />
-            Sex: {Patient.sex}
+            Sex: {patient.sex}
             <br />
-            Weight: {Patient.weight}
+            Weight: {patient.weight}
             <br />
-            Height: {Patient.height}
+            Height: {patient.height}
           </p>
         </div>
       </div>
       <div className="container mx-auto py-10">
         <h2 className="text-2xl font-bold pb-4 font-[Syne]">Recordings</h2>
-        <DataTable columns={columns} data={recordings} />
+        <DataTable columns={columns} data={patient.recordings} />
       </div>
     </div>
   );
