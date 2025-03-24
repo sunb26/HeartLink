b1c3f80726198a94c10857139525720fa04a0a02
matt-wilker <mattw5871@rogers.com>
2025-03-24 19:58:53 -0400

    server: clean file comments

--- server/handlers/uploadFilterRecording.go
+++ server/handlers/uploadFilterRecording.go
@@ -15,100 +15,7 @@ import (
 	"github.com/google/uuid"
 )
 
-// type requestBody struct { // CAN ADD "STRUCT TAGS" TO THIS IF WILL HELP WITH DECODING STRUCT
-// 	UserID       string
-// 	WavFileBytes uint64 // byte array -> a slice of uint8 values
-// }
-
-// POSTRawAudioFile function (OLD)
-/*
-func POSTRawAudioFile(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Fprint(w, "POSTRawAudioFile Endpoint - Start\n\n")
-
-	r.Body = http.MaxBytesReader(w, r.Body, 1*1024*1024) // limit the size of the request body to 1 MB
-
-	decoder := json.NewDecoder(r.Body) // create a new JSON decoder
-
-	var req requestBody // create a new requestBody struct
-
-	err := decoder.Decode(&req) // decode the JSON request body into the requestBody struct
-
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var MaxBytesError *http.MaxBytesError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
-			http.Error(w, msg, http.StatusBadRequest)
-
-		case errors.As(err, &MaxBytesError):
-			msg := fmt.Sprintf("Request body is too large. Max size is %d bytes", MaxBytesError.Limit)
-			http.Error(w, msg, http.StatusRequestEntityTooLarge)
-
-		default:
-			fmt.Printf("default error: %v\n", err)
-			log.Print(err.Error())
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		}
-		return
-	}
-
-	err = decoder.Decode(&struct{}{}) // check if there are any extra JSON objects/characters in the request body
-	if !errors.Is(err, io.EOF) {
-		msg := "Request body must only contain one JSON object"
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
-
-	// No parameters in POST endpoint to read
-
-	// --- extracting request body fields --- //
-
-	userID := req.UserID             // extract userID from request body
-	WavFileBytes := req.WavFileBytes // extract wavFileBytes from request body
-	//json.Unmarshal([]uint64(req.WavFileBytes), &req.WavFileBytes)
-
-	fmt.Fprint(w, "POSTRawAudioFile request body:\n") // Write request body to response writer "w"
-	// fmt.Fprint(w, "userID: ", userID, "\n")           // Write userID to response writer "w"
-	fmt.Printf("userID: %v\n", userID) // Write userID to response writer "w"
-	fmt.Printf("wavFileBytes: %v\n", WavFileBytes)
-
-	// fmt.Fprint(w, "wavFileBytes: ", req.WavFileBytes[0], "\n")   // Write wavFileBytes to response writer "w"
-	//fmt.Fprint(w, "wavFileBytes: ", req.WavFileBytes[1], "\n\n") // second byte in wavFileBytes
-	// fmt.Fprint(w, "wavFileBytes: ", string(wavFileBytes), "\n\n") // Write wavFileBytes to response writer "w"
-
-	// --- POST request (from ESP32 to server) requirements --- //
-
-
-
-	// --- establishing firebase connection --- //
-
-	errFB := firebasedb.FirebaseDB().Connect() // connect to firebase storage
-	if errFB != nil {
-		log.Println(errFB)
-		return
-	}
-
-	// TEST firebase connection via "get all" call
-	// testErr := firebasedb.FirebaseDB().GetAllFilesFirebase()
-	// if testErr != nil {
-	// 	log.Println(testErr)
-	// }
-
-	// r.ParseMultipartForm(10 << 20) // Limit upload size to 10MB
-
-
-
-	fmt.Fprint(w, "POSTRawAudioFile Endpoint - End\n")
-}
-*/
-
-// MIGHT RENAME THIS, BC DOES MORE THAN THIS - UploadFilterRecording
 func UploadFilterRecording(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Printf("\nReceiveMultipartForm Endpoint - Start\n\n") // TESTING
 
 	// ensure receiving POST request
 	if r.Method != "POST" {
@@ -258,5 +165,4 @@ func UploadFilterRecording(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, "UploadFilterRecording Endpoint - End\n") // TESTING
-	fmt.Printf("\nReceiveMultipartForm Endpoint - End\n")   // TESTING
 }
